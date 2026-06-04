86abfe52bb1af56d96b542555411180e274f9840
saprka0704 <soo.park418@gmail.com>
2026-06-04 04:03:38 +0000

    modified:   june4th.py

--- june4th.py
+++ june4th.py
@@ -3,5 +3,9 @@ x = float(input("what is x?"))
 y = float(input("what is y?"))
 #int: interger, float: decimal number, string: text 
 #print(x+y)  
-z=round(x+y)
-print(z)
+#z=round(x+y)
+#print(z)
+#round(number[,ndigits]): round the number on the nth digit, if ndigits is not given, it rounds to the nearest integer.
+#z=round(x/y,2) #rounds to 2 decimal places
+z = x/y
+print(f"{z:.2f}")
