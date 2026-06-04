6c291ba914583c26e6ccd5022d7da8bcfc97115e
saprka0704 <soo.park418@gmail.com>
2026-06-04 03:58:22 +0000

    new file:   howtousegithub.py 	modified:   june4th.py

---
+++ howtousegithub.py
@@ -0,0 +1,12 @@
+# 1단계: 현재 상태 확인 (변경된 파일 보기)
+#git status
+
+# 2단계: 모든 변경사항을 스테이징
+#git add .
+
+# 3단계: 커밋 (메시지는 자신이 원하는 것으로)
+#git commit -m "당신의 커밋 메시지"
+
+# 4단계: GitHub에 푸시
+#git push origin main
+print("Hello, GitHub!")
--- june4th.py
+++ june4th.py
@@ -2,5 +2,6 @@
 x = float(input("what is x?"))  
 y = float(input("what is y?"))
 #int: interger, float: decimal number, string: text 
-print(x+y)  
-
+#print(x+y)  
+z=round(x+y)
+print(z)
