7da49db945f72ae87570203f6b813b5deabc73aa
Sooah Park <soo.park418@gmail.com>
2026-08-21 01:08:10 +0000

    add 0821 python practice

---
+++ 0821.py
@@ -0,0 +1,31 @@
+#operators
+#augmented assignment operators
+number = 5*3+2
+number *=4
+print(number)
+
+#math module
+from math import *
+result=floor(4.99)
+print(result)
+result=ceil(3.14)
+print(result)
+result=sqrt(16)
+print(result)
+
+import math
+result=math.floor(4.99)
+print(result)
+result=math.ceil(3.14)
+print(result)
+result=math.sqrt(16)
+print(result)
+
+#random module
+from random import *
+print(random())
+print(random()*10)
+print(int(random()*10))
+
+print(randrange(1,46))
+print(randint(1,45))
