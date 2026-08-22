df916e019fd69efd7fc5282cc2b7f253a9473c54
Sooah Park <soo.park418@gmail.com>
2026-08-22 13:34:58 +0900

    Enhance comments in 0821.py for clarity
    Added comments to clarify the use of math and random functions.

--- 0821.py
+++ 0821.py
@@ -5,7 +5,7 @@ number *=4
 print(number)
 
 #math module
-from math import *
+from math import * #to use every function of the module 'math'
 result=floor(4.99)
 print(result)
 result=ceil(3.14)
@@ -23,9 +23,9 @@ print(result)
 
 #random module
 from random import *
-print(random())
-print(random()*10)
+print(random()) #randomly chooses any number from 0 to 1(excludes 1)
+print(random()*10) #randomly chooses any number from 0 to 10(excludes 10)
 print(int(random()*10))
 
-print(randrange(1,46))
-print(randint(1,45))
+print(randrange(1,46)) #randomly chooses an integer from 1 to 45(excludes 46)
+print(randint(1,45)) #randomly chooses an integer from 1 to 45(includes 45)
