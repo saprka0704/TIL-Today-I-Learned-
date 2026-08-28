0bf61a7917b324128040131e3ae1de9d545feac6
Sooah Park <soo.park418@gmail.com>
2026-08-28 00:56:07 +0000

    upload the file 0827-project1.py

---
+++ 0827-project1.py
@@ -0,0 +1,16 @@
+# using the shuffle(), and sample() to draw winners for the comment event
+from random import * # use the random module for shuffle and sample functions
+
+user = range(1,21) #class=range
+user = list(user)  #change the data structure to 'list'
+#print(type(user)) # can check the data structure
+
+shuffle(user) #shuffle the user list
+
+winner = sample(user,4) #draw 4 values of the list
+
+print("-- Winners Announcement --")
+print("chicken coupon winner : {}".format(winner[0]))
+print("coffee coupon winner : {0}".format(winner[1:]))
+print("-- CONGRATULATIONS! --")
+
